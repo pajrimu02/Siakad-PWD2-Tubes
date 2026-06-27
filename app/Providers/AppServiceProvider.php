a064f8068c1dfec84a1d1548084d658293817cca
pajimu02 <pajrimuhamad751@gmail.com>
2026-06-27 11:38:45 +0700

    Upload screenshots sistem siak

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
-        if ($this->app->environment('production')) {
-        \URL::forceScheme('https');
-    }
+        
     }
 }
